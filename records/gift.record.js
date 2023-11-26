92ae50f7f2272eb4ed24861220f6fb2cdafa4b35
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2023-11-26 14:42:02 +0100

    installed mysql2

---
+++ records/gift.record.js
@@ -0,0 +1,20 @@
+class GiftRecord {
+    static listAll() {
+        return [
+            {
+                id: "135333",
+                name: "Domek dla lalek",
+                count: 4,
+            },
+            {
+                id: "14634533",
+                name: "Samochodzik na pilota",
+                count: 6,
+            },
+        ]
+    }
+}
+
+module.exports ={
+    GiftRecord,
+}
