f1bc0ad7bd3e4dad6dac67888a631f194505e3cc
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2023-11-26 16:18:04 +0100

    validation and insert methoid added in GiftRecord

--- records/gift.record.js
+++ records/gift.record.js
@@ -1,20 +1,42 @@
+const {pool} = require("../utils/db");
+const {ValidationError} = require("../utils/error");
+const {v4: uuid} = require('uuid');
+
 class GiftRecord {
-    static listAll() {
-        return [
-            {
-                id: "135333",
-                name: "Domek dla lalek",
-                count: 4,
-            },
-            {
-                id: "14634533",
-                name: "Samochodzik na pilota",
-                count: 6,
-            },
-        ]
+    constructor(obj) {
+        if (!obj.name || obj.name.length < 3 || obj.name.length > 55) {
+            throw new ValidationError('nazwa prezentu musi mieć od 3 do 55 znaków.')
+        }
+
+        if (!obj.count || obj.count < 1 || obj.count > 999999) {
+            throw new ValidationError('liczba szt. prezentu powinna się mieścić w przedziale 1 - 999999..')
+        }
+
+        this.id = obj.id;
+        this.name = obj.name;
+        this.count = obj.count;
+    }
+
+    async insert() {
+        if (!this.id) {
+            this.id = uuid();
+        }
+
+        await pool.execute("INSERT INTO `gifts` VALUES(:id, :name, :count)", {
+            id: this.id,
+            name: this.name,
+            count: this.count,
+        });
+
+        return this.id;
+    }
+
+    static async listAll() {
+        const [results] = await pool.execute("SELECT * FROM `gifts`");
+        return results;
     }
 }
 
-module.exports ={
+module.exports = {
     GiftRecord,
 }
