285c09f7ad73bd6cda832632a20caacc346566c0
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2023-11-26 16:18:55 +0100

    added post to giftRouter

--- routers/gift.js
+++ routers/gift.js
@@ -3,12 +3,30 @@ const {GiftRecord} = require("../records/gift.record");
 
 const giftRouter = Router();
 
-giftRouter.get('/', (req, res) => {
-    const giftList = GiftRecord.listAll();
-    res.render('gift/list', {
-        giftList
+giftRouter
+    .get('/', async (req, res) => {
+        const giftList = await GiftRecord.listAll();
+        res.render('gift/list', {
+            giftList
+        });
+    }) // pobieramy i pokazujemy wszystkie prezenty
+
+    .post('/', async (req, res) => {
+        const data = {
+            ...req.body,
+            count: Number(req.body.count),
+        };//zmieniamy typ na liczbę
+
+        const newGift = new GiftRecord(data); // zmiana na obiekt typu record
+        await newGift.insert(); // dodajemyy
+        const giftsList = GiftRecord.listAll();  // pobiermay listę aktualną do rendera
+
+        res.redirect('/gift'); // odsyłamy do prezentów
+
+        res.render('gift/list', {
+            giftsList
+        });
     });
-});
 
 module.exports = {
     giftRouter,
