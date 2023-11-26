1e02c3438ae589c3dafc458525f027e29ee66bcf
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2023-11-26 08:27:14 +0100

    usehomeRouter in index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const {create} = require('express-handlebars');
 const methodOverride = require('method-override');
-const {homeRouter} = require("./routers/home");
-const {tasksRouter} = require("./routers/tasks");
 const {handleError} = require("./utils/error");
+const {homeRouter} = require("./routers/home");
 
 const app = express();
 
@@ -23,8 +22,12 @@ app.use(express.static('public'));
 app.engine('.hbs', hbs.engine);
 app.set('view engine', '.hbs');
 
+app.use('/', homeRouter);
+app.get('/children', (req, res) => {
+    res.render('children/list');
+})
 
-app.use(handleError);
+
 
 app.listen(3000, 'localhost', () => {
     console.log('Listening on http://localhost:3000')
