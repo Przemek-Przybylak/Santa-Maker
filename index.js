3199dbd7040128fdb62f9d9a58364eeb1f77801f
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2023-11-26 14:40:13 +0100

    gift router inplement in app

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const methodOverride = require('method-override');
 const {handleError} = require("./utils/error");
 const {homeRouter} = require("./routers/home");
 const {childRouter} = require("./routers/child");
+const {giftRouter} = require("./routers/gift");
 
 const app = express();
 
@@ -25,6 +26,7 @@ app.set('view engine', '.hbs');
 
 app.use('/', homeRouter);
 app.use('/child', childRouter);
+app.use('/gift', giftRouter);
 
 
 
