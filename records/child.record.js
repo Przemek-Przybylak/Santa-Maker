d7810d70d55c5c1ce93e7a56d6abe55e271e20fb
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2023-11-26 17:25:52 +0100

    child record added

--- records/child.record.js
+++ records/child.record.js
@@ -1,17 +1,33 @@
+const {pool} = require("../utils/db");
+const {ValidationError} = require("../utils/error");
+const {v4: uuid} = require("uuid");
+
 class ChildRecord {
-    static listAll() {
-        return [
-            {
-                id: "133",
-                name: "Ania Zielona",
-                gift: "Domek dla lalek",
-            },
-            {
-                id: "1463",
-                name: "Dominik Biały",
-                gift: "Samochodzik na pilota",
-            },
-        ]
+    constructor(obj) {
+        if (!obj.name || obj.name.length < 3 || obj.name.length > 25) {
+            throw new ValidationError('imię musi mieć od 3 do 25 znaków.')
+        }
+
+        this.id = obj.id;
+        this.name = obj.name;
+    }
+
+    async insert() {
+        if (!this.id) {
+            this.id = uuid();
+        }
+
+        await pool.execute("INSERT INTO `children`(`id`, `name`) VALUES(:id, :name)", {
+            id: this.id,
+            name: this.name,
+        });
+
+        return this.id;
+    }
+
+    static async listAll() {
+        const [results] = await pool.execute("SELECT * FROM `children` ORDER BY `name` ASC");
+        return results;
     }
 }
 
