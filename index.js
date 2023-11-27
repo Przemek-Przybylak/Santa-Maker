2229abda9c921193f050f7e11bc4f951c14d5737
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2023-11-27 19:11:45 +0100

    refactoring in index.js

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
-const express = require('express');
-require('express-async-errors');
-const {create} = require('express-handlebars');
-const methodOverride = require('method-override');
-const {handleError} = require("./utils/error");
-const {homeRouter} = require("./routers/home");
-const {childRouter} = require("./routers/child");
-const {giftRouter} = require("./routers/gift");
-require('./utils/db');
-const {handlebarsHelpers} = require("./utils/handlebars-helpers");
+const express = require("express");
+require("express-async-errors");
+const { create } = require("express-handlebars");
+const methodOverride = require("method-override");
+const { handleError } = require("./utils/error");
+const { homeRouter } = require("./routers/home");
+const { childRouter } = require("./routers/child");
+const { giftRouter } = require("./routers/gift");
+require("./utils/db");
+const { handlebarsHelpers } = require("./utils/handlebars-helpers");
 
 const app = express();
 
 const hbs = create({
-    extname: '.hbs',
-    helpers: handlebarsHelpers
+  extname: ".hbs",
+  helpers: handlebarsHelpers, //dodanie własnych helperów do handlebarsów
 });
 
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 app.use(express.json());
 
-app.use(express.urlencoded({
+app.use(
+  express.urlencoded({
     extended: true,
-}));
-app.use(express.static('public'));
+  })
+);
+app.use(express.static("public"));
 
-app.engine('.hbs' , hbs.engine);
-app.set('view engine', '.hbs');
+app.engine(".hbs", hbs.engine);
+app.set("view engine", ".hbs");
 
-app.use('/', homeRouter);
-app.use('/child', childRouter);
-app.use('/gift', giftRouter);
+app.use("/", homeRouter);
+app.use("/child", childRouter);
+app.use("/gift", giftRouter);
 //
-// app.use(handleError) do zrobienia widok erora
-app.listen(3000, 'localhost', () => {
-    console.log('Listening on http://localhost:3000')
-})
+// app.use(handleError)
+app.listen(3000, "localhost", () => {
+  console.log("Listening on http://localhost:3000");
+});
